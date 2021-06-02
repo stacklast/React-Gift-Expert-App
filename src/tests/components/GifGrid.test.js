4177681664d35a12cd5db52f4e931247b2d28256
Edwin Benalcázar <softwareywebsoluciones@gmail.com>
2021-06-02 14:57:43 -0500

    ADD: Evaluar si existen componentes

--- src/tests/components/GifGrid.test.js
+++ src/tests/components/GifGrid.test.js
@@ -19,17 +19,26 @@ describe('Prueba en <GifGrid />', () => {
     });
     
     test('debe mostrar imagenes cuando se carga useFetchGifs', () => {
-        const gifs = [{
-            id: 'ABC',
-            url: 'https://localhost/image.png',
-            title: 'cual quier cosa'
-        }];
+        const gifs = [
+            {
+                id: 'ABC',
+                url: 'https://localhost/image.png',
+                title: 'cual quier cosa'
+            },
+            {
+                id: '123',
+                url: 'https://localhost/image.png',
+                title: 'cual quier cosa'
+            }
+        ];
         useFetchGifs.mockReturnValue({
             data: gifs,
             loading: false
         });
         const wrapper =  shallow(<GifGrid category={ category } />);
         expect( wrapper ).toMatchSnapshot();
+        expect( wrapper.find('p').exists()).toBe(false);
+        expect( wrapper.find('GifGridItem').length).toBe(gifs.length);
 
     });
     
